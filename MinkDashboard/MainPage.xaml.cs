37e446cb1695a83fccf94cadfb6583bcf8f1be29
Nima abdoli <nimaabdoli@outlook.com>
2021-08-29 22:29:56 +0430

    Add project files.

---
+++ MinkDashboard/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// my added library
+using Windows.Devices.Bluetooth.Rfcomm;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using Windows.Devices.Bluetooth;
+
+// from sample project
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Windows.Devices.Enumeration;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Media.Imaging;
+
+
+namespace MinkDashboard
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void SendTest_Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
